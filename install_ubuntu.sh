ba45d1841129d6e3ea659ef016e94eba85d1cea3
Antoine Baudrand <antoine.baudrand@epitech.eu>
2017-09-16 18:51:08 +0200

    Remove comments

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -41,52 +41,24 @@ export LC_ALL=en_US.UTF-8;
 locale-gen en_US.UTF-8
 DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
 
-# cat << EOF > /etc/network/interfaces
-# auto lo
-# iface lo inet loopback
-# EOF
-
 echo ubuntu > /etc/hostname
 echo "127.0.0.1       $(cat /etc/hostname)" >> /etc/hosts
 
 cat << EOF > /etc/apt/sources.list
-# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
-# newer versions of the distribution.
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE" main restricted
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE" main restricted
-## Major bug fix updates produced after the final release of the
-## distribution.
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-updates main restricted
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-updates main restricted
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team, and may not be under a free licence. Please satisfy yourself as to
-## your rights to use the software. Also, please note that software in
-## universe WILL NOT receive any review or updates from the Ubuntu security
-## team.
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE" universe
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE" universe
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-updates universe
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-updates universe
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team, and may not be under a free licence. Please satisfy yourself as to
-## your rights to use the software. Also, please note that software in
-## multiverse WILL NOT receive any review or updates from the Ubuntu
-## security team.
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE" multiverse
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE" multiverse
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-updates multiverse
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-updates multiverse
-## N.B. software from this repository may not have been tested as
-## extensively as that contained in the main release, although it includes
-## newer versions of some applications which may provide useful features.
-## Also, please note that software in backports WILL NOT receive any review
-## or updates from the Ubuntu security team.
 deb http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-backports main restricted universe multiverse
 # deb-src http://fr.archive.ubuntu.com/ubuntu/ "$RELEASE"-backports main restricted universe multiverse
-## Uncomment the following two lines to add software from Canonical's
-## 'partner' repository.
-## This software is not part of Ubuntu, but is offered by Canonical and the
-## respective vendors as a service to Ubuntu users.
 # deb http://archive.canonical.com/ubuntu "$RELEASE" partner
 # deb-src http://archive.canonical.com/ubuntu "$RELEASE" partner
 
